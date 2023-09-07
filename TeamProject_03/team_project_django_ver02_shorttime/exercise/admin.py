6ee00d5175d9fd99ce5a21b474bbf0009e7cd9bb
Limkyuchan <dennis0108@hanmail.net>
2023-09-07 09:55:04 +0900

    23/09/07 Team Project 마무리 및 과정 종료 > 최종 프로젝트 장고 수정

--- TeamProject_03/team_project_django_ver02_shorttime/exercise/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import UploadFile, ImgModel, Exercise
-
-# Register your models here.
-
-admin.site.register(UploadFile)
-admin.site.register(ImgModel)
-admin.site.register(Exercise)
